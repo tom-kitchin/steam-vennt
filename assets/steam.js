6d3e5b14c957b8f97175d7c23427460df8226d66
Tom Kitchin <tom.kitchin@create.net>
2017-09-08 15:44:09 +0100

    Move rate limit handler and skip check for obvious vanity id

--- assets/steam.js
+++ assets/steam.js
@@ -10,7 +10,7 @@ function isSteamKeySet () {
 }
 
 function resolveVanityUrl (steamVanityId) {
-  return axios.get('https://api.steampowered.com/ISteamUser/ResolveVanityURL/v1/', {
+  return requestQueue.schedule(axios.get, 'https://api.steampowered.com/ISteamUser/ResolveVanityURL/v1/', {
     params: { key: process.env.STEAM_API_KEY, vanityurl: steamVanityId }
   }).then(({ data }) => {
     if (data.response.success !== 1) {
@@ -22,7 +22,11 @@ function resolveVanityUrl (steamVanityId) {
 }
 
 function getOwnedGames (steamId) {
-  return axios.get('https://api.steampowered.com/IPlayerService/GetOwnedGames/v1/', {
+  if (!/^\d+$/.test(steamId)) {
+    // If it's not all numbers, it's probably not a steam ID!
+    return Promise.reject(new Error(`${steamId} doesn't look like a valid Steam ID`))
+  }
+  return requestQueue.schedule(axios.get, 'https://api.steampowered.com/IPlayerService/GetOwnedGames/v1/', {
     params: { key: process.env.STEAM_API_KEY, steamid: steamId, include_appinfo: '1' }
   }).then(({ data }) => {
     if (_.isEmpty(data.response)) {
@@ -36,10 +40,10 @@ function getOwnedGames (steamId) {
 let server = {
   getSteamOwnedGames (steamId) {
     if (isSteamKeySet()) {
-      return requestQueue.schedule(getOwnedGames, steamId).catch(() => {
+      return getOwnedGames(steamId).catch(() => {
         // Maybe it's a vanity ID?
-        return requestQueue.schedule(resolveVanityUrl, steamId).then((response) => {
-          return requestQueue.schedule(getOwnedGames, response.steamid).then((response) => {
+        return resolveVanityUrl(steamId).then((response) => {
+          return getOwnedGames(response.steamid).then((response) => {
             return response
           })
         }).catch((e) => {
