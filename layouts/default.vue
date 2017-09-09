86f6fbd684228377001f8d72e94482d1aae55dc2
Tom Kitchin <tkitchin@gmail.com>
2017-09-10 00:14:55 +0100

    Fix responsiveness, whoops

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,67 +1,9 @@
 <template>
   <div>
     <header class="container">
-      <h1><a href="/"><img :src="require('~/assets/images/novenntwithoutsteam-small.png')" alt="Vennt logo" /></a></h1>
+      <h1><a href="/"><img :src="require('~/assets/images/novenntwithoutsteam-small.png')" alt="Vennt logo" class="img-fluid" /></a></h1>
     </header>
     <hr />
     <nuxt/>
   </div>
 </template>
-
-<style>
-.container {
-  line-height: 1.5em;
-  margin-left: 1em;
-  margin-right: 1em;
-}
-
-.fa-icon {
-  width: auto;
-  height: 1em; /* or any other relative font sizes */
-}
-
-/*html {
-  font-family: "Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif;
-  font-size: 16px;
-  word-spacing: 1px;
-  -ms-text-size-adjust: 100%;
-  -webkit-text-size-adjust: 100%;
-  -moz-osx-font-smoothing: grayscale;
-  -webkit-font-smoothing: antialiased;
-  box-sizing: border-box;
-}
-
-*, *:before, *:after {
-  box-sizing: border-box;
-  margin: 0;
-}
-
-.button--green {
-  display: inline-block;
-  border-radius: 4px;
-  border: 1px solid #3b8070;
-  color: #3b8070;
-  text-decoration: none;
-  padding: 10px 30px;
-}
-
-.button--green:hover {
-  color: #fff;
-  background-color: #3b8070;
-}
-
-.button--grey {
-  display: inline-block;
-  border-radius: 4px;
-  border: 1px solid #35495e;
-  color: #35495e;
-  text-decoration: none;
-  padding: 10px 30px;
-  margin-left: 15px;
-}
-
-.button--grey:hover {
-  color: #fff;
-  background-color: #35495e;
-}*/
-</style>
