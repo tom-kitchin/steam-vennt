8f310eca5df16e2d5e42cd4d3b621c59c6d2ae4e
Tom Kitchin <tkitchin@gmail.com>
2017-09-13 20:35:45 +0100

    Switch sqlite3 lib

--- server/db.js
+++ server/db.js
@@ -1,11 +1,28 @@
-const sqlite3 = require('sqlite3').verbose()
+const Database = require('better-sqlite3')
+import _ from 'lodash'
 
 const DBPATH = 'vennt.sqlite3'
 
 module.exports = {
   serialize (call) {
-    let db = new sqlite3.Database(DBPATH)
+    let db = new Database(DBPATH)
     db.serialize(call(db))
+    db.close()
+  },
+  initialize () {
+    let db = new Database(DBPATH)
+    db.prepare('CREATE TABLE games (id INTEGER PRIMARY KEY, appid INTEGER, name TEXT)').run()
+    db.prepare('CREATE TABLE tags (id INTEGER PRIMARY KEY, name TEXT)').run()
+    db.prepare('CREATE TABLE games_tags (id INTEGER PRIMARY KEY, game_id INTEGER, tag_id INTEGER)').run()
+    db.close()
+  },
+  addGame ({ appId, name, tags }) {
+    let db = new Database(DBPATH)
+    let game = db.prepare('INSERT INTO games VALUES (@appid, @name)').run({ appid, name })
+    _.each(tags, (tag) => {
+      let tag = db.prepare('INSERT INTO tags VALUES (?)').run(tag)
+      db.prepare('INSERT INTO games_tags VALUES (@gameId, @tagId)').run({ gameId: game.lastInsertROWID, tagId: tag.lastInsertROWID })
+    })
     db.close()
   }
 }
